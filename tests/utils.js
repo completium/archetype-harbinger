041f5de5812010a95c79264a61805102897401b1
Benoit Rognier <benoit.rognier@edukera.com>
2022-07-02 15:45:17 +0200

    fix

--- tests/utils.js
+++ tests/utils.js
@@ -1,8 +1,37 @@
 const { isMockup, getValueFromBigMap, exprMichelineToJson, packTyped, blake2b, sign, keccak } = require('@completium/completium-cli');
 
-const string_type= {
+// LIB ------------------------------------------------------------------------
+
+exports.string_type = {
   prim: "string"
 }
+
+exports.make_string = v => {
+  return { "string" : v }
+}
+
+exports.make_int = v => {
+  return { "int": v }
+}
+
+exports.make_prim = (p, a) => {
+  return {
+    prim: p,
+    args: a
+  }
+}
+
+exports.make_elt       = (a, b) => this.make_prim("Elt",  [ a, b ])
+exports.make_pair      = (a, b) => this.make_prim("Pair", [ a, b ])
+exports.make_pair_type = (a, b) => this.make_prim("pair", [ a, b ])
+
+exports.make_map = l => {
+  return l.map( x => {
+    return this.make_elt(x.key, x.value)
+  })
+}
+
+// ORACLE objects
 
 exports.make_asset_value_oracleData = (start, end, open, high, low, close, volume) => {
   return  {
@@ -58,7 +87,7 @@ exports.make_asset_value_oracleData = (start, end, open, high, low, close, volum
   }
 }
 
-const asset_value_oracleData_type = {
+exports.asset_value_oracleData_type = {
   "prim": "pair",
   "args": [
     {
@@ -131,50 +160,12 @@ const asset_value_oracleData_type = {
   ]
 }
 
-exports.asset_value_oracleData_type = asset_value_oracleData_type
-
-const make_string = v => {
-  return { "string" : v }
-}
-
-exports.make_string = make_string
-
-const make_int = v => {
-  return { "int": v }
-}
-
-const make_prim = (p, a) => {
-  return {
-    prim: p,
-    args: a
-  }
-}
-
-const make_elt       = (a, b) => make_prim("Elt",  [ a, b ])
-const make_pair      = (a, b) => make_prim("Pair", [ a, b ])
-const make_pair_type = (a, b) => make_prim("pair", [ a, b ])
-
-const make_map = l => {
-  return l.map( x => {
-    return make_elt(x.key, x.value)
-  })
-}
-
-exports.make_map = make_map
-
-exports.make_pair = make_pair
-
-exports.make_pair_type = make_pair_type
-
-const make_update_upm_key   = a => make_string(a)
-const make_update_upm_value = (a, b) => make_pair(make_string(a), b)
-
-exports.make_update_upm_key = make_update_upm_key
-exports.make_update_upm_value = make_update_upm_value
+exports.make_update_upm_key = a => this.make_string(a)
+exports.make_update_upm_value = (a, b) => this.make_pair(this.make_string(a), b)
 
 exports.sign_update_data = async (key, data, account) => {
-  const value = make_pair(key, data)
-  const type  = make_pair_type(string_type, asset_value_oracleData_type)
+  const value = this.make_pair(key, data)
+  const type  = this.make_pair_type(this.string_type, this.asset_value_oracleData_type)
   const packed = packTyped(value, type)
   const signed = await sign(packed, { as: account.name })
   return signed.prefixSig
