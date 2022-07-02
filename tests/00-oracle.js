bab5368c819a3a8bcc213d63a59a69010a144c3d
Benoit Rognier <benoit.rognier@edukera.com>
2022-07-02 19:18:15 +0200

    fix

--- tests/00-oracle.js
+++ tests/00-oracle.js
@@ -1,38 +1,92 @@
+/* Imports ----------------------------------------------------------------- */
+
 const {
   deploy,
   expectToThrow,
-  exprMichelineToJson,
   getAccount,
-  getValueFromBigMap,
-  jsonMichelineToExpr,
-  runGetter,
-  packTyped,
   setEndpoint,
   setMockupNow,
   setQuiet,
 } = require('@completium/completium-cli');
 const {
   oracle,
-  sign_update_data,
-  make_update_upm,
-  get_asset_value_oracleData
-} = require('./utils');
+  sign_oracle_data,
+  sign_oracle_revoke,
+  oracle_state,
+  errors
+} = require('./oracle');
 const assert = require('assert');
 
-// accounts
-const alice = getAccount('alice');
+/* Accounts ---------------------------------------------------------------- */
 
-//set endpointhead
+const alice = getAccount('alice');
+const bob   = getAccount('bob');
+
+/* Endpoint ---------------------------------------------------------------- */
+
 setEndpoint('mockup');
 
-// verbose mode
+/* Verbose mode ------------------------------------------------------------ */
+
 setQuiet(true);
 
-// now
+/* Now --------------------------------------------------------------------- */
+
 const now = Math.floor(Date.now() / 1000)
 setMockupNow(now)
 
-// scenario
+/* Data -------------------------------------------------------------------- */
+
+const asset1          = "XTZ-USD"
+const asset2          = "BTC-USD"
+const asset_untracked = "XTZ-BTC"
+const input1  = {
+  start  : '1970-01-01T00:00:01Z',
+  end    : '1970-01-01T00:00:02Z',
+  open   : 3,
+  high   : 4,
+  low    : 5,
+  close  : 6,
+  volume : 7
+}
+const input2  = {
+  start  : '1970-01-01T00:00:08Z',
+  end    : '1970-01-01T00:00:09Z',
+  open   : 10,
+  high   : 11,
+  low    : 12,
+  close  : 13,
+  volume : 14
+}
+const input_past  = {
+  start  : '1970-01-01T00:00:08Z',
+  end    : '1970-01-01T00:00:09Z',
+  open   : 15,
+  high   : 16,
+  low    : 17,
+  close  : 18,
+  volume : 19
+}
+const input3 = {
+  start  : '1970-01-01T00:00:15Z',
+  end    : '1970-01-01T00:00:16Z',
+  open   : 17,
+  high   : 18,
+  low    : 19,
+  close  : 20,
+  volume : 21
+}
+const input4 = {
+  start  : '1970-01-01T00:00:22Z',
+  end    : '1970-01-01T00:00:23Z',
+  open   : 24,
+  high   : 25,
+  low    : 26,
+  close  : 27,
+  volume : 28
+}
+
+/* Scenario ---------------------------------------------------------------- */
 
 describe('[Oracle] Contract deployment', async () => {
   it('Deploy Oracle', async () => {
@@ -48,24 +102,99 @@ describe('[Oracle] Contract deployment', async () => {
   });
 })
 
-describe('[Oracle] Update once with valid data', async () => {
-  it('Update Oracle', async () => {
-    const asset = "XTZ-USD"
-    const input  = {
-      start  : '1970-01-01T00:00:01Z',
-      end    : '1970-01-01T00:00:02Z',
-      open   : 3,
-      high   : 4,
-      low    : 5,
-      close  : 6,
-      volume : 7
-    }
-    const sig   = await sign_update_data(asset, input, alice)
-    await oracle.update([ { key: asset, value: [ sig, input ] } ], {
+describe('[Oracle] Update', async () => {
+  it('Update once with valid data', async () => {
+    const sig = await sign_oracle_data(asset1, input1, alice)
+    await oracle.update([ { key: asset1, value: [ sig, input1 ] } ], {
       as: alice.pkh
     })
-    const output = await oracle.get_oracleData(asset);
-    assert(JSON.stringify(input,0,2) == JSON.stringify(output,0,2))
+    const output = await oracle.get_oracleData(asset1);
+    assert(JSON.stringify(output, 0, 2) == JSON.stringify(input1, 0, 2))
+  })
+  it('Second Update Overwrites First Update', async () => {
+    const sig = await sign_oracle_data(asset1, input2, alice)
+    await oracle.update([ { key: asset1, value: [ sig, input2 ] } ], {
+      as: alice.pkh
+    })
+    const output = await oracle.get_oracleData(asset1);
+    assert(JSON.stringify(output, 0, 2) == JSON.stringify(input2, 0, 2))
+  })
+  it('Correctly Processes Updates With Data From The Past', async () => {
+    const sig = await sign_oracle_data(asset1, input_past, alice)
+    await oracle.update([ { key: asset1, value: [ sig, input_past ] } ], {
+      as: alice.pkh
+    })
+    const output = await oracle.get_oracleData(asset1);
+    assert(JSON.stringify(output, 0, 2) == JSON.stringify(input2, 0, 2))
+  })
+  it('Untracked Asset does not update oracle', async () => {
+    const sig = await sign_oracle_data(asset_untracked, input1, alice)
+    await oracle.update([ { key: asset_untracked, value: [ sig, input1 ] } ], {
+      as: alice.pkh
+    })
+    const output = await oracle.get_oracleData(asset_untracked);
+    assert(output == undefined)
+  })
+  it('Update Fails With Bad Signature', async () => {
+    const sig = await sign_oracle_data(asset1, input3, bob)
+    expectToThrow(async () => {
+      await oracle.update([ { key: asset1, value: [ sig, input3 ] } ], {
+        as: alice.pkh
+      })
+    }, errors.INVALID_SIG)
+  })
+  it('Update with stale asset does not fail', async () => {
+    const sig1 = await sign_oracle_data(asset1, input3, alice)
+    const sig2 = await sign_oracle_data(asset2, input1, alice)
+    await oracle.update([
+      { key: asset1, value: [ sig1, input3 ] },
+      { key: asset2, value: [ sig2, input1 ] },
+    ], {
+      as: alice.pkh
+    })
+    const output1 = await oracle.get_oracleData(asset1);
+    assert(JSON.stringify(output1, 0, 2) == JSON.stringify(input3, 0, 2))
+    const output2 = await oracle.get_oracleData(asset2);
+    assert(JSON.stringify(output2, 0, 2) == JSON.stringify(input1, 0, 2))
+    const sig3 = await sign_oracle_data(asset1, input4, alice)
+    await oracle.update([
+      { key: asset1, value: [ sig3, input4 ] },
+      { key: asset2, value: [ sig2, input1 ] },
+    ], {
+      as: alice.pkh
+    })
+    const output3 = await oracle.get_oracleData(asset1);
+    assert(JSON.stringify(output3, 0, 2) == JSON.stringify(input4, 0, 2))
+    const output4 = await oracle.get_oracleData(asset2);
+    assert(JSON.stringify(output4, 0, 2) == JSON.stringify(input1, 0, 2))
   })
 })
 
+describe('[Oracle] Revoke', async () => {
+  it('Incorrect Revoke Fails to Revoke An Oracle', async () => {
+    const sig = await sign_oracle_revoke(bob)
+    expectToThrow(async () => {
+      await oracle.revoke(sig, {
+        as : alice.pkh
+      })
+    }, errors.INVALID_SIG)
+  })
+  it('Revoke Oracle', async () => {
+    const sig = await sign_oracle_revoke(alice)
+    await oracle.revoke(sig, {
+      as : alice.pkh
+    })
+    const state = await oracle.get_state()
+    assert(state == oracle_state.Revoked)
+    const output = await oracle.get_oracleData(asset1);
+    assert(output == undefined)
+  })
+  it('Update Fails when Revoked', async () => {
+    const sig = await sign_oracle_data(asset1, input3, alice)
+    expectToThrow(async () => {
+      await oracle.update([ { key: asset1, value: [ sig, input3 ] } ], {
+        as: alice.pkh
+      })
+    }, errors.REVOKED)
+  })
+})
--- tests/oracle.js
+++ tests/oracle.js
@@ -1,37 +1,13 @@
-const { isMockup, getValueFromBigMap, exprMichelineToJson, packTyped, blake2b, sign, keccak, getStorage } = require('@completium/completium-cli');
+/* Imports ---------------------------------------------------------- */
 
-// LIB ------------------------------------------------------------------------
+const {
+  getValueFromBigMap,
+  packTyped,
+  sign
+} = require('@completium/completium-cli');
+const Micheline = require('./micheline')
 
-exports.string_type = {
-  prim: "string"
-}
-
-exports.make_string = v => {
-  return { "string" : v }
-}
-
-exports.make_int = v => {
-  return { "int": v }
-}
-
-exports.make_prim = (p, a) => {
-  return {
-    prim: p,
-    args: a
-  }
-}
-
-exports.make_elt       = (a, b) => this.make_prim("Elt",  [ a, b ])
-exports.make_pair      = (a, b) => this.make_prim("Pair", [ a, b ])
-exports.make_pair_type = (a, b) => this.make_prim("pair", [ a, b ])
-
-exports.make_map = l => {
-  return l.map( x => {
-    return this.make_elt(x.key, x.value)
-  })
-}
-
-// ORACLE objects -------------------------------------------------------------
+/* ORACLE objects ---------------------------------------------------------- */
 
 exports.make_asset_value_oracleData = (asset_value_oracle) => {
   return  {
@@ -160,18 +136,26 @@ exports.asset_value_oracleData_type = {
   ]
 }
 
-exports.make_update_upm_key = a => this.make_string(a)
-exports.make_update_upm_value = (a, b) => this.make_pair(this.make_string(a), this.make_asset_value_oracleData(b))
+exports.make_update_upm_key = a => Micheline.make_string(a)
+exports.make_update_upm_value = (a, b) => Micheline.make_pair(Micheline.make_string(a), Micheline.make_asset_value_oracleData(b))
+
+const cmp = (a,b) => {
+  if (a.key.string === b.key.string) {
+    return 0;
+  }
+  return a.key.string < b.key.string ? -1 : 1;
+};
+
 exports.make_update_upm = l => {
-  return this.make_map(l.map(x => {
+  return Micheline.make_map(l.map(x => {
     return {
-      key   : this.make_string(x.key),
-      value : this.make_pair(this.make_string(x.value[0]), this.make_asset_value_oracleData(x.value[1]))
+      key   : Micheline.make_string(x.key),
+      value : Micheline.make_pair(Micheline.make_string(x.value[0]), this.make_asset_value_oracleData(x.value[1]))
     }
-  }))
+  }), cmp)
 }
 
-// Utils ----------------------------------------------------------------------
+/* Contract ---------------------------------------------------------------- */
 
 const oracle = new Object()
 
@@ -183,29 +167,73 @@ oracle.update = async (a, p) => {
   });
 }
 
+oracle.revoke = async (a, p) => {
+  await oracle.contract.revoke({
+    argJsonMichelson: Micheline.make_string(a),
+    as: p.as,
+    amount: p.amount
+  });
+}
+
 oracle.get_oracleData = async key => {
   const storage = await oracle.contract.getStorage()
   const data = await getValueFromBigMap(
     parseInt(storage.oracleData),
-    this.make_string(key),
-    this.string_type)
-  return {
-    start  : data.args[0].string,
-    end    : data.args[1].string,
-    open   : parseInt(data.args[2].int, 10),
-    high   : parseInt(data.args[3].int, 10),
-    low    : parseInt(data.args[4].int, 10),
-    close  : parseInt(data.args[5].int, 10),
-    volume : parseInt(data.args[6].int, 10)
+    Micheline.make_string(key),
+    Micheline.string_type)
+  if (data != undefined) {
+    return {
+        start  : data.args[0].string,
+        end    : data.args[1].string,
+        open   : parseInt(data.args[2].int, 10),
+        high   : parseInt(data.args[3].int, 10),
+        low    : parseInt(data.args[4].int, 10),
+        close  : parseInt(data.args[5].int, 10),
+        volume : parseInt(data.args[6].int, 10)
+    }
+  } else {
+    return undefined
+  }
+}
+
+exports.oracle_state = {
+  Running : 0,
+  Revoked : 1
+}
+
+oracle.get_state = async () => {
+  const storage = await oracle.contract.getStorage()
+  const state = storage._state
+  if (state.toNumber() == 0) {
+    return this.oracle_state.Running
+  } else {
+    return this.oracle_state.Revoked
   }
 }
 
 exports.oracle = oracle
 
-exports.sign_update_data = async (key, data, account) => {
-  const value = this.make_pair(this.make_string(key), this.make_asset_value_oracleData(data))
-  const type  = this.make_pair_type(this.string_type, this.asset_value_oracleData_type)
+/* Utils ------------------------------------------------------------------- */
+
+exports.sign_oracle_data = async (key, data, account) => {
+  const value = Micheline.make_pair(Micheline.make_string(key), this.make_asset_value_oracleData(data))
+  const type  = Micheline.make_pair_type(Micheline.string_type, this.asset_value_oracleData_type)
   const packed = packTyped(value, type)
   const signed = await sign(packed, { as: account.name })
   return signed.prefixSig
+}
+
+exports.sign_oracle_revoke = async (account) => {
+  const value = Micheline.none;
+  const type  = Micheline.make_option_type(this.key_type)
+  const packed = packTyped(value, type)
+  const signed = await sign(packed, { as: account.name })
+  return signed.prefixSig
+}
+
+/* Errors ------------------------------------------------------------------ */
+
+exports.errors = {
+  INVALID_SIG : "bad sig",
+  REVOKED     : "revoked"
 }
